494055e77ec749fa2e02eda728dc9a6625d2bcc0
Ronald Bogdanoff <ron@Ronalds-MacBook-Pro.local>
2018-01-24 17:11:58 -0800

    commit work

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # jenkinsfarm
 Containerized Jenkins Farm
+
+Requires: Docker (I am using Docker for Mac)
+
+Uses gulp for utility tasks (start, stop, etc).  
+- requires node.js
+- requires npm
+- requires gulp-cli 'npm install --global gulp-cli'
+
+Then you can install dependencies
+- npm install
---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const gulp = require('gulp');
+const spawn = require('child_process').spawn;
+
+const jenkinsVer = '2.102';
+
+gulp.task('default', defaultTask);
+
+gulp.task('build', function() {
+    return spawn('docker', ['build', '-t', 'jenkinsfarmmaster', './jenkins'], {stdio: 'inherit'})
+});
+
+gulp.task('start', function() {
+    return spawn('docker', ['run', '-p', '8080:8080', '--name=jenkins-master', '-d', 'jenkinsfarmmaster'], {stdio: 'inherit'})
+});
+
+gulp.task('stop', function() {
+    return spawn('docker', ['rm', '-f', 'jenkins-master'], {stdio: 'inherit'})
+});
+
+gulp.task('list', function() {
+    return spawn('docker', ['ps', '-a'], {stdio: 'inherit'})
+});
+
+gulp.task('password', function() {
+    return spawn('docker', ['exec', 'jenkins-master', 'cat', '/var/jenkins_home/secrets/initialAdminPassword'], {stdio: 'inherit'})
+});
+
+function defaultTask(done) {
+  // place code for your default task here
+  done();
+}
